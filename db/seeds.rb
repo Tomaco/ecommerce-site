46906c366b25cf9925c99652af7747b0956a6c6b
Tomaco <tomgdieguez@gmail.com>
2016-02-03 20:21:17 -0300

    Add custom style

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => "Containers", :description => "Ceramic, plastic & themed containers for your crop.", :image_url => "pots.jpg", :size => "Large", :price => "100" )
+
+Product.create(:name => "Soil Mix", :description => "The perfect combination of grow mediums including all the necessary micro-nutrients for your plants.", :image_url => "soilmix.jpg", :size => "Large", :price => "50" )
+
+Product.create(:name => "Pruner", :description => "Cut, trim & crop with this lovely tool.", :image_url => "pruner.jpg", :size => "Small", :price => "8" )
+
+Product.create(:name => "Vermicompost", :description => "The rounded wormy fellas from west coast got this produce ready to feed your plants.", :image_url => "vermicompost.jpg", :size => "Medium", :price => "25" )
