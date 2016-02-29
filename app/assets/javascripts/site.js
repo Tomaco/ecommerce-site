e3cfb41d77e4d1fbe07c7dafd16a3089b5f4723e
Tomaco <tomgdieguez@gmail.com>
2016-02-29 18:59:59 -0300

    Modify indentation

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 // Star Raty System below
 $(document).on('ready page:load', function(){
-	$('.rating').raty( { path: '../assets/raty', scoreName: 'comment[rating]' });
-	$('.rated').raty( { path: '../assets/raty', readOnly: 'true', score: function(){
-		return $(this).attr('data-score');}
-	});
+  $('.rating').raty( { path: '../assets/raty', scoreName: 'comment[rating]' });
+  $('.rated').raty( { path: '../assets/raty', readOnly: 'true', score: function(){
+    return $(this).attr('data-score');}
+  });
 });
