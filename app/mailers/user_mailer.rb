7da4b9ae0527483718ebaa6b113e3f5795fb852d
Tomaco <tomgdieguez@gmail.com>
2016-03-07 14:22:21 -0300

    Add automated email functionality on user sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ApplicationMailer
-	default from: "contact@tomgrow.com"
+  default from: "contact@tomgrow.com"
 
-	def contact_form(email, name, message)
-	  @message = message
-	    mail(:from => email,
-	        :to => 'td.csiamcorp@gmail.com',
-	        :subject => "A new contact form message from #{name}")
-	  end
+  def contact_form(email, name, message)
+    @message = message
+      mail(:from => email,
+          :to => 'td.csiamcorp@gmail.com',
+          :subject => "A new contact form message from #{name}")
+    end
+
+  def welcome(user)
+    @appname = "Tom's Growshop"
+    mail( :to => user.email,
+          :subject => "Welcome to #{@appname}!")
+  end
+
 end
