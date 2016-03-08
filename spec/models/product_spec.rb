e78dc3d07a21bc4f68b8a64afd4399c14612a109
Tomaco <tomgdieguez@gmail.com>
2016-03-08 18:25:30 -0300

    Create spec file for Product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Product do
+  
+  context "when the product has comments" do
+    before do
+      @product = Product.create!(name: "containers")
+      @user = User.create!(email: "johng@gmail.com", password: "fake1234")
+      @comments = @product.comments.create!(rating: 1, user: @user, body: "Awful container!"), @product.comments.create!(rating: 3, user: @user, body: "Average container!"), @product.comments.create!(rating: 5, user: @user, body: "Great container!")
+    end
+
+    it "returns the average rating of all comments" do
+      expect(@product.avg_rating).to eq 3
+    end
+  end
+
+  context "when creating a product" do
+    before do
+      @product = Product.new(description: "Containers of all sizes")
+    end
+
+    it "is not valid" do
+      expect(@product).not_to be_valid
+    end
+  end
+
+end
