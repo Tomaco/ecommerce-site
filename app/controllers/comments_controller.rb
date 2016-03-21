434ae1605ffe6af275adbd824e69ccb7d9380f4b
Tomaco <tomgdieguez@gmail.com>
2016-03-21 16:06:20 -0300

    Allow AJAX request on create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,4 @@
-class CommentsController < ApplicationController
+  class CommentsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
